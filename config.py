a7724f42418ca2e6792724171febf4d61ae4c507
Ranjan098 <144373098+Ranjan098@users.noreply.github.com>
2024-04-18 13:16:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ api_id = 12606917
 
 api_hash = "f25113b8c17dca6fa7abda53a86bd4f7"
 
-bot_token = "6608265446:AAGNIZJF7OpScs5udEDEklyoqMrEiK2Qbhk"
+bot_token = "6608265446:AAFopFtRVFWcNWHYmjDMRAWjytI0AQHrATU"
 
